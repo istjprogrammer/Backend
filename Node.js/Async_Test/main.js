b233a50c59dd7244f05c083b7a525dbd9e3af037
istjprogrammer <139939895+istjprogrammer@users.noreply.github.com>
2023-11-29 11:54:44 +0900

    [Node.js] 반복문에서 비동기 처리

--- Node.js/Async_Test/main.js
+++ Node.js/Async_Test/main.js
@@ -140,3 +140,30 @@ const wrap = async() => {
 }
 wrap()
 */
+
+//반복문에서 비동기 처리
+const getMovies = (movieName) =>{
+    return new Promise((resolve) => {
+    fetch(`https://www.omdbapi.com/?apikey=7035c60c&s=${movieName}`)
+        //fetch로 데이터를 받아온 다음 then으로 함수 돌려주기  
+        .then(res => res.json())
+        .then(json => resolve(json))
+    })
+}
+
+const titles = ['frozen', 'avengers', 'avatar']
+
+/*
+titles.forEach(async title =>{
+    const movies = await getMovies(title)
+    console.log(title, movies)
+})
+*/
+
+const wrap = async ()=>{
+    for(const title of titles){
+    const movies = await getMovies(title)
+    console.log(title, movies)
+    }
+}
+wrap()
