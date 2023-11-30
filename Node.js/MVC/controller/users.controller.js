ac701a5e17e6eb8934afaf627dca4373fab888b6
istjprogrammer <139939895+istjprogrammer@users.noreply.github.com>
2023-11-30 11:37:17 +0900

    [Node.js] MVC패턴 : 컨트롤러 생성

---
+++ Node.js/MVC/controller/users.controller.js
@@ -0,0 +1,30 @@
+//모든 사용자 조회
+function getUsers(req, res) {
+    res.send(Users);
+}
+
+//특정 사용자 조회
+function getUser(req, res) {
+    const userId = req.params.userId
+    const user = Users[userId]    
+    if (user)
+        res.json(user);
+    //잘못된 아이디 전송시 404에러 출력
+    else
+        res.sendStatus(404).end();
+}
+
+//사용자 추가 기능
+function postUser(req, res) {
+    if (!req.body.name) {
+        res.status(400).json({ error: "Missing user name" })
+    }
+
+    const newUser = {
+        name: req.body.name,
+        id: Users.length
+    }
+
+    Users.push(newUser);
+    res.json(newUser);
+}
