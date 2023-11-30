60fecdff32a24433725e9d3a896a2493f8485059
istjprogrammer <139939895+istjprogrammer@users.noreply.github.com>
2023-11-30 16:41:19 +0900

    [Node.js] MVC패턴 : Controller(users 수정)

--- Node.js/MVC/controller/users.controller.js
+++ Node.js/MVC/controller/users.controller.js
@@ -1,30 +1,26 @@
 const Users = require("../models/users.model")
 
-//모든 사용자 조회
-function getUsers(req, res) {
+function getUsers(req, res){
     res.send(Users);
 }
 
-//특정 사용자 조회
-function getUser(req, res) {
-    const userId = req.params.userId
-    const user = Users[userId]    
-    if (user)
+function getUser(req, res){
+    const userId = req.params.userId;
+    const user = Users[userId];
+    if(user)
         res.json(user);
-    //잘못된 아이디 전송시 404에러 출력
     else
         res.sendStatus(404).end();
 }
 
-//사용자 추가 기능
-function postUser(req, res) {
-    if (!req.body.name) {
-        res.status(400).json({ error: "Missing user name" })
+function postUser(req, res){
+    if(!req.body.name){
+        return res.status(400).json({error: "Missing user name"});  
     }
 
     const newUser = {
-        name: req.body.name,
-        id: Users.length
+        name : req.body.name,
+        id : Users.length
     }
 
     Users.push(newUser);
@@ -34,5 +30,5 @@ function postUser(req, res) {
 module.exports = {
     getUser,
     getUsers,
-    postUser   
+    postUser
 }
